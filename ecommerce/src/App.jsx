2cdbf7756c31eb8afb55e6d60bee6b7ee07d6497
Bereket Gebreyesus <51488182+Bereket-Gebreyesus@users.noreply.github.com>
2024-06-14 12:10:12 +0200

    Update App.jsx
    Refactor App.jsx

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,28 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import productsData from './fake-data/all-products.js';
-import categoriesData from './fake-data/all-categories.js';
-import CategoryList from './components/CategoryList.jsx';
-import ProductList from './components/ProductList.jsx';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DisplayProductsBasedOnCategory from './DisplayProductsBasedOnCategory';
+import ProductDetail from './components/ProductDetail';
 
-const App = () => {
-  const [selectedCategory, setSelectedCategory] = useState(null);
-
-  const handleSelectCategory = (categoryId) => {
-    setSelectedCategory(categoryId);
-  };
-
-  // Filter products based on selected category
-  const filteredProducts = selectedCategory
-    ? productsData.filter(product => product.categoryIds.includes(selectedCategory))
-    : productsData;
-
-  return (
-    <div className="App">
-      <CategoryList categories={categoriesData} selectedCategory={selectedCategory} onSelectCategory={handleSelectCategory} />
-      <ProductList products={filteredProducts} />
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<DisplayProductsBasedOnCategory />} />
+      <Route path="/product/:id" element={<ProductDetail />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
